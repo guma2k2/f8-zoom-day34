d6388d5ca859d25978c2344ea6e611583cc93760
Thuan ngo <thuanngo3072002@gmail.com>
2025-08-28 18:07:46 +0700

    finish homework

--- main.js
+++ main.js
@@ -1,21 +1,39 @@
-const h1 = React.createElement("h1", null, "Todo List");
-
-const ul = React.createElement(
-    "ul",
-    {
-        id: "task-list",
-    },
-    [
-        React.createElement("li", {
-            className: "task-item active",
-            title: "Task 1",
-        }),
-        React.createElement("li", {
-            className: "task-item completed",
-            title: "Task 2",
-        }),
-    ]
+const html = (
+    <>
+        <h1>Bài tập về nhà</h1>
+        <ul>
+            <li>
+                <a href="./counter.html">
+                    Bài 1: <strong>Counter App</strong>
+                </a>
+            </li>
+            <li>
+                <a href="./todo.html">
+                    Bài 2: <strong>Todo App</strong>
+                </a>
+            </li>
+            <li>
+                <a href="./profile.html">
+                    Bài 3: <strong>Profile App</strong>
+                </a>
+            </li>
+            <li>
+                <a href="./products.html">
+                    Bài 4: <strong>Product List</strong>
+                </a>
+            </li>
+            <li>
+                <a href="./comments.html">
+                    Bài 5: <strong>Comment List</strong>
+                </a>
+            </li>
+            <li>
+                <a href="./weather.html">
+                    Bài 6: <strong>Weather App</strong>
+                </a>
+            </li>
+        </ul>
+    </>
 );
 const root = ReactDOM.createRoot(document.querySelector("#root"));
-root.render(h1);
-root.render(ul);
+root.render(html);
